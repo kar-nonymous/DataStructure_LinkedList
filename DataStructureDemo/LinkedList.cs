86a02acab7aea5eeb8f9dc797daa30d58ce981f0
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-10-23 20:53:51 +0530

    [Kartikeya] Linked list implementation to add data

---
+++ DataStructureDemo/LinkedList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureDemo
+{
+    public class LinkedList
+    {
+        public Node head;
+        /// <summary>
+        /// UC1
+        /// Inserts the last.
+        /// </summary>
+        /// <param name="new_data">The new data.</param>
+        public void InsertLast(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            if (this.head == null)
+                this.head = new_node;
+            else
+            {
+                Node lastNode = GetLastNode();
+                lastNode.next = new_node;
+            }
+            Console.WriteLine("Insert into list " + new_node.data);
+        }
+        /// <summary>
+        /// Gets to the last node
+        /// </summary>
+        /// <returns></returns>
+        public Node GetLastNode()
+        {
+            Node temp = this.head;
+            while (temp.next != null)
+                temp = temp.next;
+            return temp;
+        }
+        /// <summary>
+        /// Displays linked list content
+        /// </summary>
+        public void Display()
+        {
+            Node temp = this.head;
+            if(temp==null)
+            {
+                Console.WriteLine("Linked is empty");
+                return;
+            }
+            else
+            {
+                while(temp!=null)
+                {
+                    Console.Write(" " + temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
---
+++ DataStructureDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DataStructureDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.InsertLast(56);
+            list.InsertLast(30);
+            list.InsertLast(70);
+        }
+    }
+}
